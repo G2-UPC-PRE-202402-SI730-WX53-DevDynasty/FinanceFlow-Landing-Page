b2f5cc104cede78aa566afb1de83f17d042362f1
Ricardo <richiabv@gmail.com>
2024-09-12 02:18:01 -0500

    feat(scripts): added language script

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,3 +4,12 @@ const navMenu = document.getElementById('nav-menu');
 menuToggle.addEventListener('click', () => {
     navMenu.classList.toggle('active');
 });
+function changeLanguage(lang) {
+    if (lang === 'es') {
+        window.location.href = 'index-es.html';
+    }
+    if (lang === 'en') {
+        window.location.href = 'index.html';
+    }
+
+}
